b880aba48d054b6a9dd557233a92f381c11f6777
tdellorussoCV <38885677+tdellorussoCV@users.noreply.github.com>
2018-10-01 18:06:49 -0400

    10/1
    Fixed header to form right size
    
    Started Left NAV

--- Angular_Version/src/js/app/leftnav/leftnav.component.js
+++ Angular_Version/src/js/app/leftnav/leftnav.component.js
@@ -3,9 +3,8 @@ angular.module('leftnav').
 component('leftnav', {
     templateUrl: './templates/leftnav.html',
     controller: function($scope) {
-        $scope.clfbutton = function() {
-            $("input.logfiles").removeClass("ng-hide").addClass('ng-show');
-            $("button.logfiles").removeClass("ng-hide").addClass('ng-show');
-        }
+        $scope.clfbutton = function($event) {
+           
+           }
     }
 });
